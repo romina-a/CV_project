b98f328eac4c4a6abe452332b1516f0ef02042a7
elie0174 <36248350+elie0174@users.noreply.github.com>
2020-11-12 18:55:56 -0500

    Create DataInputTest.py

---
+++ DataInputTest.py
@@ -0,0 +1,25 @@
+DIRECTORY = ""
+CATEGORIES = ["with_mask", "without_mask"]
+
+
+data = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
+
+    	data.append(image)
+    	labels.append(category)
+
+# use one-hot encoding on the labels so we dont have a much of strings as are labels 
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
