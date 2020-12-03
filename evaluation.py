f06c9631e8ab0877f234453549293c6633ea6ecd
Romina <rominaabadi1994@gmail.com>
2020-12-03 15:48:36 -0500

    updated plot files, added script for recreating the plots

--- evaluation.py
+++ evaluation.py
@@ -6,11 +6,12 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import load_model
 from detector import violaJones_face_detection, dnn_face_detection
 import os
-from sklearn.metrics import classification_report, precision_recall_curve, roc_auc_score
+from sklearn.metrics import classification_report, precision_recall_curve, \
+    roc_auc_score, average_precision_score, auc
 from sklearn.preprocessing import LabelBinarizer
 from tensorflow.keras.utils import to_categorical
 
-# face detection options (Just to make to code clean)
+# face detection options (Just to make the code clean)
 fd_options = {
     'ViolaJones': violaJones_face_detection,
     'DNN': dnn_face_detection
@@ -98,3 +99,102 @@ def test_model_on_test_images(classifier_path=None, fd_method='DNN', test_direct
         plt.grid
 
     return labels, pred_labels
+
+
+def script():
+    C123_path = "./experiments/classifier123/classifier123.model"
+    C5_path = "./experiments/classifier5/classifier5.model"
+    C_all_path = "./experiments/classifier_all/classifier_all.model"
+    lbls, C123_probs = test_model_on_test_images(C123_path)
+    lbls, C5_probs = test_model_on_test_images(C5_path)
+    lbls, C_all_probs = test_model_on_test_images(C_all_path)
+
+    precision123, recall123, thresholds123 = \
+        precision_recall_curve(y_true=lbls.argmax(axis=1), probas_pred=C123_probs[:, 1], pos_label=1)
+    precision5, recall5, thresholds5 = \
+        precision_recall_curve(y_true=lbls.argmax(axis=1), probas_pred=C5_probs[:, 1], pos_label=1)
+    precision_all, recall_all, thresholds_all = \
+        precision_recall_curve(y_true=lbls.argmax(axis=1), probas_pred=C_all_probs[:, 1], pos_label=1)
+
+    # finding threshold index
+    opt_ind123 = np.where(recall123 >= 0.97)
+    opt_ind5 = np.where(recall5 >= 0.97)
+    opt_ind_all = np.where(recall_all >= 0.97)
+
+    opt_ind123 = opt_ind123[-1]
+    opt_ind5 = opt_ind5[-1]
+    opt_ind_all = opt_ind_all[-1]
+    # pr curves
+    plt.plot(recall123[:-1], precision123[:-1], label="C-123")
+    plt.plot(recall_all[:-1], precision_all[:-1], label="C-all")
+    plt.plot(recall5[:-1], precision5[:-1], label="C-5")
+    plt.plot(recall123[(opt_ind123[-1])], precision123[(opt_ind123[-1])], 'xk')
+    plt.plot(recall5[(opt_ind5[-1])], precision5[(opt_ind5[-1])], 'xk')
+    plt.plot(recall_all[(opt_ind_all[-1])], precision_all[(opt_ind_all[-1])], 'xk')
+    plt.legend()
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.savefig("./prcurves.png")
+    plt.show()
+
+    # individual precision and recall against threshold
+    plt.plot(thresholds123, precision123[:-1], label="precision")
+    plt.plot(thresholds123, recall123[:-1], label="recall")
+    plt.plot(thresholds123[opt_ind123[-1]], precision123[opt_ind123[-1]], 'xk')
+    plt.plot(thresholds123[opt_ind123[-1]], recall123[opt_ind123[-1]], 'xk')
+    plt.xlabel("Threshold")
+    plt.title("C-123 precision and recall curve")
+    plt.legend()
+    plt.savefig("./p&rcurve_c_123.png")
+    plt.show()
+
+    plt.plot(thresholds5, precision5[:-1], label="precision")
+    plt.plot(thresholds5, recall5[:-1], label="recall")
+    plt.plot(thresholds5[opt_ind5[-1]], precision5[opt_ind5[-1]], 'xk')
+    plt.plot(thresholds5[opt_ind5[-1]], recall5[opt_ind5[-1]], 'xk')
+    plt.xlabel("Threshold")
+    plt.legend()
+    plt.title("C-5 precision and recall curve")
+    plt.savefig("./p&rcurve_c_5.png")
+    plt.show()
+
+    plt.plot(thresholds_all, precision_all[:-1], label="precision")
+    plt.plot(thresholds_all, recall_all[:-1], label="recall")
+    plt.plot(thresholds_all[opt_ind_all[-1]], precision_all[opt_ind_all[-1]], 'xk')
+    plt.plot(thresholds_all[opt_ind_all[-1]], recall_all[opt_ind_all[-1]], 'xk')
+    plt.xlabel("Threshold")
+    plt.legend()
+    plt.title("C-all precision and recall curve")
+    plt.savefig("./p&rcurve_c_all.png")
+    plt.show()
+
+
+    print("AP C-123: {}"
+          .format(average_precision_score(y_true=lbls.argmax(axis=1),
+                                          y_score=C123_probs[:, 1],
+                                          pos_label=1)))
+    print("AP C-5: {}"
+          .format(average_precision_score(y_true=lbls.argmax(axis=1),
+                                          y_score=C5_probs[:, 1],
+                                          pos_label=1)))
+    print("AP C-all: {}"
+          .format(average_precision_score(y_true=lbls.argmax(axis=1),
+                                          y_score=C_all_probs[:, 1],
+                                          pos_label=1)))
+    print("AUC-ROC C-123: {}"
+          .format(roc_auc_score(y_true=lbls.argmax(axis=1),
+                                y_score=C123_probs[:, 1])))
+    print("AUC-ROC C-5: {}"
+          .format(roc_auc_score(y_true=lbls.argmax(axis=1),
+                                y_score=C5_probs[:, 1])))
+    print("AUC-ROC C-all: {}"
+          .format(roc_auc_score(y_true=lbls.argmax(axis=1),
+                                y_score=C_all_probs[:, 1])))
+    print("AUC-PR C-123: {}"
+          .format(auc(recall123, precision123)))
+    print("AUC-PR C-5: {}"
+          .format(auc(recall5, precision5)))
+    print("AUC_PR C-all: {}"
+          .format(auc(recall_all, precision_all)))
+
+script()
